e7e5a7855c2256b8262354cc78da3e092d3d6699
Chris Bellis <153526875+chrisjbellis@users.noreply.github.com>
2025-02-25 13:56:27 +0000

    updated burger-menu.js to vanilla js, added portfolio pages

--- burger-menu.js
+++ burger-menu.js
@@ -1,18 +1,24 @@
-$(document).ready(function() {
-    $('#header-container').load('/header.html', function() {
-        // Ensure the burger menu script runs after the header is loaded
-        const burgerIcon = document.getElementById('burger-icon');
-        const navContainer = document.getElementById('nav-container');
+document.addEventListener("DOMContentLoaded", function() {
+    // Load the header.html content into the header-container
+    fetch('/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-container').innerHTML = data;
 
-        if (burgerIcon && navContainer) {
-            burgerIcon.addEventListener('click', function() {
-                navContainer.classList.toggle('show');
-                burgerIcon.classList.toggle('active'); // Toggle the active class on click
-                console.log('Burger icon clicked, nav-container class toggled');
-                console.log('navContainer classes:', navContainer.classList);
-            });
-        } else {
-            console.error('Burger icon or nav container not found in the DOM.');
-        }
-    });
+            // Ensure the burger menu script runs after the header is loaded
+            const burgerIcon = document.getElementById('burger-icon');
+            const navContainer = document.getElementById('nav-container');
+
+            if (burgerIcon && navContainer) {
+                burgerIcon.addEventListener('click', function() {
+                    navContainer.classList.toggle('show');
+                    burgerIcon.classList.toggle('active'); // Toggle the active class on click
+                    console.log('Burger icon clicked, nav-container class toggled');
+                    console.log('navContainer classes:', navContainer.classList);
+                });
+            } else {
+                console.error('Burger icon or nav container not found in the DOM.');
+            }
+        })
+        .catch(error => console.error('Error loading header.html:', error));
 });
